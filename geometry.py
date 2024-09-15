cad70eaa8be6ee07ddd7b4f85887411ed2214794
milesmercer04 <116983808+milesmercer04@users.noreply.github.com>
2024-09-14 23:49:00 -0700

    Update geometry.py

---
+++ geometry.py
@@ -0,0 +1,18 @@
+
+def class Line(float x_1, float x_2, float y_1, float y_2):
+    '''
+    
+    '''
+    def __init__(self, x_1=0.0, x_2=1.0, y_1=0.0, y_2=1.0):
+        self.x_1 = x_1
+        self.x_2 = x_2
+        self.y_1 = y_1
+        self.y_2 = y_2
+        
+    def __str__(self):
+        return f'<%f, %f, %f, %f>', (self.x_1, self.x_2, self.y_1, self.y_2)
+    
+    if __name__ == '__main__'
+        pass
+    
+    
